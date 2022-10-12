6e537e66ac502b576324e2d33bba9cd53449a227
lab <lab@lab.lab>
2022-10-12 21:19:30 +1000

    Action of logon and logout working. Next set up the DB.

--- checkencryp/flask_website/app/__init__.py
+++ checkencryp/flask_website/app/__init__.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template, request
+from unicodedata import name
+from flask import Flask, render_template, request, redirect, url_for
 #from app import app
 
 app = Flask(__name__)
 
-@app.route("/", methods = ['POST', 'GET'])
+@app.route('/')
 def index():
+        return render_template("index.html")
+
+@app.route("/loggedin", methods = ['POST', 'GET'])
+def loggedin():
     if request.method == 'POST':
         user = request.form['user']
         passw = request.form['pass']
-        return render_template('loggedin.html', user = user)
-    else:
-        return render_template('index.html')
+        return render_template('loggedin.html', user = user, passw = passw)
 
-@app.route("/loggedin", methods = ['POST', 'GET'])
-def logon():
-        return render_template('index.html')
+@app.route("/logout", methods = ['POST', 'GET'])
+def logout():
+    return redirect('/')
