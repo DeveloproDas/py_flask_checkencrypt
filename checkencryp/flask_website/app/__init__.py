2e65dca464ca44ebe9be704ace5ca2add34422df
lab <lab@lab.lab>
2022-10-12 20:08:26 +1000

    Second html loggedin.html working. Method POST working. Next DB.

--- checkencryp/flask_website/app/__init__.py
+++ checkencryp/flask_website/app/__init__.py
@@ -1,8 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 #from app import app
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        user = request.form['user']
+        passw = request.form['pass']
+        return render_template('loggedin.html', user = user)
+    else:
+        return render_template('index.html')
+
+@app.route("/loggedin", methods = ['POST', 'GET'])
+def logon():
+        return render_template('index.html')
