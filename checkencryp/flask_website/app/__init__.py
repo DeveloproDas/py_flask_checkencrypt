375df6104387737e62bf685c63d0cfec0750f3aa
lab <lab@lab.lab>
2022-11-04 22:20:30 +1000

    Correction, db can work staing in folder app/, it needs absolute path. Modifications on __py__.init and index.

--- checkencryp/flask_website/app/__init__.py
+++ checkencryp/flask_website/app/__init__.py
@@ -2,12 +2,13 @@
 from flask import Flask, render_template, request, redirect, url_for, abort, Response
 import sqlite3 as sql
 from sqlite3 import Error
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-        return render_template("index.html")
+    return render_template("index.html", meta_data = meta_data)
 
 @app.route("/loggedin", methods = ['POST', 'GET'])
 def loggedin():
@@ -16,7 +17,8 @@ def loggedin():
         passw = request.form['pass']
         
         try:
-            conn = sql.connect('credentials.db')
+            #conn = sql.connect('credentials.db') #Absolute path no relative
+            conn = sql.connect(os.path.abspath("credentials.db"))
             #conn.row_factory = sql.Row
             cur = conn.cursor()
             cur.execute("SELECT * FROM tableUser")
@@ -27,10 +29,11 @@ def loggedin():
                 passwR = row[2]
         except Error as e:
             #abort(Response('User, Password or both are incorrect'))
-            abort(Response('Database can not open.' +
+            abort(Response('Database can not open.' + str(e) +
             '<br><br><button onclick="history.back()">Back</button>'))
         if user == userR and passw == passwR:
-            return render_template('loggedin.html', user = userR, passw = passwR)
+            return render_template('loggedin.html', meta_data = meta_data,
+                                user = userR, passw = passwR)
         else:
             abort(Response('User or Password incorrects.' +
             '<br><br><button onclick="history.back()">Back</button>'))
@@ -38,3 +41,5 @@ def loggedin():
 @app.route("/logout", methods = ['POST', 'GET'])
 def logout():
     return redirect('/')
+
+meta_data = ["<Course>", "<Subject>", "<Subcode>", "<LectureName>", "<StudentName>", "<studentID>"]
